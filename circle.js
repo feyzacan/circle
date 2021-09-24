4043ec51c51a42283aac26a4c2c4e7f6f5e7fe96
Feyza Yiğiter <feyzayigiter@gmail.com>
2021-09-24 08:26:17 +0300

    initial commit

---
+++ circle.js
@@ -0,0 +1,13 @@
+const circleArea = function(r) {
+   return Math.PI * r * r
+}
+
+
+const circleCircumference = function(r) {
+   return 2 * Math.PI * r
+}
+
+module.exports = {circleArea, 
+    circleCircumference
+}
+
---
+++ circle2.js
@@ -0,0 +1,5 @@
+const {circleArea, circleCircumference} = require('./circle');
+
+
+console.log(circleArea(5))
+console.log(circleCircumference(5))
